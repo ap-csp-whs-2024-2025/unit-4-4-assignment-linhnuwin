90f7da937cefe8d42ae28d10ae676803d008f91d
linhnuwin <linhnguyen091208@gmail.com>
2024-12-31 23:44:16 +0000

    4.4 Exercise A

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,16 @@
 
 int main()
 {
-    // You can follow along with any code in class here
+    // Problem 1
+    double number1;
     
-    return 0;
+    std::cout << "Pick a Number from 1 to 100: ";
+    std::cin >> number1;
+
+    while (number1 > 100 || number1 < 1)
+    {
+        std::cout << "Invalid Number";
+        std::cin >> number1;
+    }
+    std::cout << "Thank you for your timely response!";
 }
